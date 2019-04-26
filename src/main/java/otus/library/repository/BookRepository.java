71acc10ab48b705c9b5d21f44b350d27087cb954
anton <psycore@mail.ru>
2019-04-26 17:41:00 +0300

    добавил entitygraph для методов

--- src/main/java/otus/library/repository/BookRepository.java
+++ src/main/java/otus/library/repository/BookRepository.java
@@ -1,7 +1,12 @@
 package otus.library.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import otus.library.domain.Book;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @EntityGraph("book-entity-graph")
+    List<Book> findAll();
 }
--- src/main/java/otus/library/repository/CommentRepository.java
+++ src/main/java/otus/library/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package otus.library.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import otus.library.domain.Comment;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @EntityGraph("comment-entity-graph")
+    List<Comment> findAll();
 }
