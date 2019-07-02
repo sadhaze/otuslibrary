a23be40725a15686f2d961a85d3bc38b73d254fa
anton <psycore@mail.ru>
2019-07-02 17:28:15 +0300

    допилил тест для авторконтроллера

--- src/test/java/otus/library/rest/AuthorControllerTest.java
+++ src/test/java/otus/library/rest/AuthorControllerTest.java
@@ -2,48 +2,24 @@ package otus.library.rest;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import otus.library.domain.Author;
 import otus.library.repository.*;
-import reactor.core.CoreSubscriber;
-import reactor.core.publisher.Flux;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-//@RunWith(AuthorController.class)
 @SpringBootTest
-//@WebMvcTest(AuthorController.class)
 @ActiveProfiles("test")
 @DisplayName("Тест контроллера авторов")
 public class AuthorControllerTest {
-    /*@Autowired
-    private MockMvc mvc;*/
-
     @Autowired
     @Qualifier("authorsRoutes")
     private RouterFunction route;
 
-    @MockBean
+    @Autowired
     private AuthorRepository authorRepository;
 
     private final String strFname = "testFname";
@@ -52,7 +28,7 @@ public class AuthorControllerTest {
 
     @Test
     @DisplayName("Проверка страницы /flux/authors")
-    public void authorListPageTest() throws Exception {
+    public void authorsRoutesTest() throws Exception {
         WebTestClient client = WebTestClient
                 .bindToRouterFunction(route)
                 .build();
@@ -61,84 +37,43 @@ public class AuthorControllerTest {
                 .uri("/flux/authors?lname=" + strLname + "&fname=" + strFname)
                 .exchange()
                 .expectStatus()
-                .isOk();
+                .isOk()
+                .expectBody()
+                .json("{\"fname\":\"" + strFname + "\",\"lname\":\"" + strLname + "\"}");
 
         client.get()
                 .uri("/flux/authors")
                 .exchange()
                 .expectStatus()
-                .isOk();
+                .isOk()
+                .expectBody()
+                .json("[{\"fname\":\"" + strFname + "\",\"lname\":\"" + strLname + "\"}]");
 
-/*        mvc.perform(get("/flux/authors").contentType(MediaType.TEXT_HTML))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].fname", is(strFname)))
-                .andExpect(jsonPath("$[0].lname", is(strLname)));*/
+        String testId = authorRepository.findAll().blockFirst().getId();
+
+        client.put()
+                .uri("/flux/authors/" + testId + "?fname=" + strLname + "&lname=" + strFname)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .json("{\"fname\":\"" + strLname + "\",\"lname\":\"" + strFname + "\"}");
+
+        client.delete()
+                .uri("/flux/authors/" + testId)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .json("");
+
+        client.get()
+                .uri("/flux/authors")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .json("[]");
+
     }
-
-   /* @Test
-    @DisplayName("Проверка страницы /authors/edit")
-    public void authorEditTest() throws Exception {
-        Optional<Author> optionalAuthor = Optional.ofNullable(author);
-
-        given(authorRepository.findById("testId")).willReturn(optionalAuthor);
-
-        mvc.perform(get("/authors/edit")
-                    .contentType(MediaType.TEXT_HTML)
-                    .param("id", "testId"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("authors/edit"))
-                .andExpect(model().attribute("authors", optionalAuthor.get()));
-    }
-
-    @Test
-    @DisplayName("Проверка страницы /authors/create")
-    public void authorCreateTest() throws Exception {
-        given(authorRepository.save(new Author(strFname, strLname))).willReturn(author);
-
-        mvc.perform(post("/authors/create")
-                    .contentType(MediaType.TEXT_HTML)
-                    .param("fname", strFname)
-                    .param("lname", strLname))
-                .andExpect(status().isOk())
-                .andExpect(view().name("save"))
-                .andExpect(model().attribute("backref", "/authors"));
-    }
-
-    @Test
-    @DisplayName("Проверка страницы /authors/delete")
-    public void authorDeleteTest() throws Exception {
-        mvc.perform(post("/authors/delete")
-                    .contentType(MediaType.TEXT_HTML)
-                    .param("id", "testId"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("save"))
-                .andExpect(model().attribute("backref", "/authors"));
-    }
-
-    @Test
-    @DisplayName("Проверка страницы /authors/new")
-    public void authorNewTest() throws Exception {
-        mvc.perform(get("/authors/new").contentType(MediaType.TEXT_HTML))
-                .andExpect(status().isOk())
-                .andExpect(view().name("authors/new"));
-    }
-
-    @Test
-    @DisplayName("Проверка страницы /authors/save")
-    public void authorSaveTest() throws Exception {
-        Optional<Author> optionalAuthor = Optional.ofNullable(author);
-
-        given(authorRepository.findById("testId")).willReturn(optionalAuthor);
-
-        mvc.perform(post("/authors/save")
-                    .contentType(MediaType.TEXT_HTML)
-                    .param("id", "testId")
-                    .param("fname", strFname)
-                    .param("lname", strLname))
-                .andExpect(status().isOk())
-                .andExpect(view().name("save"))
-                .andExpect(model().attribute("backref", "/authors"));
-    }*/
 }
